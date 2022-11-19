9ba874de77d48182eaa575fc363287ee3d4d646b
benjaminburton <benjaminlburton@gmail.com>
2022-11-19 16:28:49 -0500

    updated js function

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,8 +1,20 @@
 // Counter Function for Cloud Resume Challenge (CRC)
+window.addEventListener('DOMContentLoaded', (e) => {
+    getVisitorCount();
+})
 
-fetch("http://lavell.com/counter.json")
-  // Fuction beginning
-  .then((res) => res.json())
-  // Fetching response
-  .then((data) => console.log(data));
-// Returning response
+const functionApi = '';
+
+const getVisitorCount = () => {
+    let counter = 10;
+    fetch(functionApi).then((res) => {
+        return res.json()
+    }).then((res) => {
+        console.log("called website");
+        counter = res.counter;
+        document.getElementById("counter").innerText = counter;
+    }).catch(function (err) {
+        console.log(err);
+    });
+    return counter;
+}
