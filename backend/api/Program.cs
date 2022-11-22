aee17c00ceecaf3708428f765896b01ec1a6e885
benjaminburton <benjaminlburton@gmail.com>
2022-11-22 15:59:24 -0500

    added cosmosDB tabel api beginnings

---
+++ backend/api/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+using Azure.Data.Tables;
+
+// New instance of the TableClient class
+TableServiceClient tableServiceClient = new TableServiceClient(Environment.GetEnvironmentVariable("COSMOS_CONNECTION_STRING"));
+
+// New instance of TableClient class referencing the server-side table
+TableClient tableClient = tableServiceClient.GetTableClient(
+    tableName: "adventureworks"
+);
+
+await tableClient.CreateIfNotExistsAsync();
