c2e5bcd29f60509cc07385d9e5f48eee6f5330e3
benjaminburton <benjaminlburton@gmail.com>
2022-12-09 23:14:01 -0500

    added python for azure http function

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,14 +1,20 @@
+import axios from "axios";
+import countapi from "countapi-js";
+
+countapi.visits().then((result) => {
+  console.log(result.value);
+});
 // Counter Function for Cloud Resume Challenge (CRC)
 
 window.addEventListener("DOMContentLoaded", (e) => {
   getVisitorCount();
 });
 
-const apiUrl = "";
+const apinode = "https://apinode.documents.azure.com:443/";
 
 const getVisitorCount = () => {
-  let count = 10;
-  fetch(apiUrl)
+  let count = 0;
+  fetch("https://apinode.documents.azure.com:443/")
     .then((response) => {
       return response.json();
     })
@@ -22,3 +28,9 @@ const getVisitorCount = () => {
     });
   return count;
 };
+
+// module.exports = async function (context, req) {
+//   context.res.json({
+//     text: "Hello from the API",
+//   });
+// };
